9dd8be2fa70b0b4e5f8dcad43e856269b6c90b8e
Patryk Wołosewicz <bazant988@gmail.com>
2022-05-29 08:12:19 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ router.post('/login', passport.authenticate('local',{
   }
   else if(req.user.role==='ADMIN')
   { 
-   res.redirect('/crudplumber');
+   res.redirect('https://bookerapppg.herokuapp.com/crudplumber');
    
   }
   
